fa5b5d184d20c7bcdfe71909e8922ee2f1c29a22
Terrance A. Snyder <terrance.a.snyder@gmail.com>
2011-10-23 16:48:02 -0400

    Update to reduce default memory for container.

--- shared/template/bin/setenv.sh
+++ shared/template/bin/setenv.sh
@@ -2,8 +2,8 @@
 
 # discourage address map swapping by setting Xms and Xmx to the same value
 # http://confluence.atlassian.com/display/DOC/Garbage+Collector+Performance+Issues
-export CATALINA_OPTS="$CATALINA_OPTS -Xms1024m"
-export CATALINA_OPTS="$CATALINA_OPTS -Xmx1024m"
+export CATALINA_OPTS="$CATALINA_OPTS -Xms512m"
+export CATALINA_OPTS="$CATALINA_OPTS -Xmx512m"
 
 # Increase maximum perm size for web base applications to 4x the default amount
 # http://wiki.apache.org/tomcat/FAQ/Memoryhttp://wiki.apache.org/tomcat/FAQ/Memory
